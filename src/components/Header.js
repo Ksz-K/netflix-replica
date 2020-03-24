6bf51a9c5c9f083df60711585c36a370c0dbc4b2
Krzysztof Szachna-Krause <kszk@gmx.us>
2020-03-24 22:17:24 +0100

    initial setup

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import logo from "../svg/logo.svg";
+import { NavLink } from "react-router-dom";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header-container">
+        <div className="header-top">
+          <img src={logo} alt="" />
+          <NavLink to="/" className="signIn-btn">
+            Sign In
+          </NavLink>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
