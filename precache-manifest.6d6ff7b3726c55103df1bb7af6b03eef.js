cda8297a8b0805e7817fb470a13c67146b7d24bb
Krzysztof Szachna-Krause <kszk@gmx.us>
2020-05-04 12:03:24 +0200

    Updates

---
+++ precache-manifest.6d6ff7b3726c55103df1bb7af6b03eef.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ceeba7b92bafb907fe1279011789a3c",
+    "url": "/netflix-replica/index.html"
+  },
+  {
+    "revision": "b0e55846d5e3e1fb5ba8",
+    "url": "/netflix-replica/static/css/main.06e94f25.chunk.css"
+  },
+  {
+    "revision": "163518cee687ba252a1d",
+    "url": "/netflix-replica/static/js/2.f39c179a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/netflix-replica/static/js/2.f39c179a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e55846d5e3e1fb5ba8",
+    "url": "/netflix-replica/static/js/main.b98e903a.chunk.js"
+  },
+  {
+    "revision": "f3c612d29e98d1bf3576",
+    "url": "/netflix-replica/static/js/runtime-main.76b55c05.js"
+  },
+  {
+    "revision": "dc2411c98b969c2d71b0920ae3e2d9d8",
+    "url": "/netflix-replica/static/media/bg.dc2411c9.jpg"
+  },
+  {
+    "revision": "cadb5e001e0a014a8bedbd9e74381f95",
+    "url": "/netflix-replica/static/media/logo.cadb5e00.svg"
+  },
+  {
+    "revision": "fab25bc378670766a10e1ec37712945e",
+    "url": "/netflix-replica/static/media/tab-1-pic.fab25bc3.png"
+  },
+  {
+    "revision": "66c7c1fb5b71b2deaff929bbcd3bacf7",
+    "url": "/netflix-replica/static/media/tab-macbook.66c7c1fb.png"
+  },
+  {
+    "revision": "dc986d819c053deb6180a34320af2988",
+    "url": "/netflix-replica/static/media/tab-tablet.dc986d81.png"
+  },
+  {
+    "revision": "9f91203b7c374a590dd727c323d5b6dc",
+    "url": "/netflix-replica/static/media/tab-tv.9f91203b.png"
+  }
+]);
